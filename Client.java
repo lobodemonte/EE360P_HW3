10c2f36b4900b43db9a76eb5a805dfe06aacbe12
stubbs12 <stubbs12@utexas.edu>
2015-03-01 16:35:05 -0600

    UDP kind of done?

--- Client.java
+++ Client.java
@@ -1,5 +1,9 @@
+import java.net.*; import java.io.*; import java.util.Scanner;
 
 public class Client {
+	
+	private static int clientID;
+	private static String addressIP;
 	
 	static void main (String args[]) {
 		
@@ -13,8 +17,7 @@ public class Client {
 			throw new IllegalArgumentException("Must provide <clientID ip_server>");
 		}
 		
-		int clientID;
-		String addressIP = parts[1];
+		addressIP = parts[1];
 		
 		try {
 			clientID = Integer.parseInt(parts[0]);
@@ -31,6 +34,85 @@ public class Client {
 		
 		//valid
 		
+		for(int i = 1; i < args.length; i++) {
+			parts = args[i].split(" ");
+			
+			if(parts.length == 2 && parts[0].equals("sleep")) {
+				long sleeptime = 0;
+				
+				try {
+					sleeptime = Long.parseLong(parts[1]);
+				}
+				catch(NumberFormatException e) {
+					continue;
+				}
+				
+				try {
+					Thread.sleep(sleeptime);
+				} catch (InterruptedException e) {
+					continue;
+				}
+			}
+			else if(parts.length == 4) {
+				int portNum = 0;
+				
+				try {
+					portNum = Integer.parseInt(parts[2]);
+				} catch (NumberFormatException e) {
+					continue;
+				}
+				
+				if(parts[3].equals("U")) { //UDP
+					
+					//clientNum b# reserve|return
+			        String message = "" + clientID + " " + parts[0] + " " + parts[1];
+					
+					String returnMsg = sendUDP(portNum, message);
+				}
+				else if(parts[3].equals("T")) { //TCP
+					
+				}
+				
+				
+			}
+			else {
+				//TODO: something??
+			}
+			
+			
+		}
+		
+	}
+	
+	private static String sendUDP(int port, String message) {
+		String hostname = addressIP;
+		String retstring = null;
+        int len = 1024;
+        byte[] rbuffer = new byte[len];
+        DatagramPacket sPacket, rPacket;
+        
+        try {
+            InetAddress ia = InetAddress.getByName(hostname);
+            DatagramSocket datasocket = new DatagramSocket();
+            Scanner sc = new Scanner(System.in);
+            
+            byte[] buffer = new byte[message.length()];
+            buffer = message.getBytes();
+            sPacket = new DatagramPacket(buffer, buffer.length, ia, port);
+            datasocket.send(sPacket);            	
+            rPacket = new DatagramPacket(rbuffer, rbuffer.length);
+            datasocket.receive(rPacket);
+            retstring = new String(rPacket.getData(), 0,
+            		rPacket.getLength());
+        } catch (UnknownHostException e) {
+            System.err.println(e);
+        } catch (SocketException e) {
+            System.err.println(e);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+        
+        return retstring;
 	}
 
 }
--- Server.java
+++ Server.java
@@ -1,5 +1,9 @@
+import java.net.*;
+import java.io.*;
 
 public class Server {
+	
+	private static int bookAvail[];
 	
 	static void main (String args[]) {
 		
@@ -30,14 +34,129 @@ public class Server {
 		
 		//input is valid!!
 		
-		boolean bookAvail[] = new boolean[numBooks];
+		bookAvail = new int[numBooks];
 		for(int i = 0; i < bookAvail.length; i++) {
-			bookAvail[i] = true;
+			bookAvail[i] = 0;
 		}
 		
 		//SERVER STUFF:
-		//TODO:
+		//TODO: separate thread for TCP
+		
+		//UDP:
+		
+		DatagramPacket datapacket, returnpacket;
+		datapacket = null;
+		byte[] returnMsg = null;
+		int len = 1024; //TODO: need to change/set this ?????
+		
+		try {
+			DatagramSocket datasocket = new DatagramSocket(portUDP);
+			byte[] buf = new byte[len];
+			while (true) {
+				
+				//if statement basically allows me to skip over this on the first time
+				if(returnMsg != null) {
+					returnpacket = new DatagramPacket(
+							returnMsg,
+							returnMsg.length,
+							datapacket.getAddress(),
+							datapacket.getPort());
+					datasocket.send(returnpacket);
+				}
+				
+				datapacket = new DatagramPacket(buf, buf.length);
+				datasocket.receive(datapacket);
+			
+				////////////////
+				
+				String msg = new String(datapacket.getData());
+				String msgParts[] = msg.split(" ");
+				
+				if(msgParts.length != 3) {
+					returnMsg = "error".getBytes();
+					continue;
+				}
+				
+				if(msgParts[1].length() < 2 || msgParts[0].charAt(0) != 'b') {
+					returnMsg = "error".getBytes();
+					continue;
+				}
+				
+				int bookNum, clientNum;
+				
+				try {
+					clientNum = Integer.parseInt(msgParts[0]);
+					bookNum = Integer.parseInt(msgParts[1].substring(1));
+				}
+				catch(NumberFormatException e) {
+					returnMsg = "error".getBytes();
+					continue;
+				}
+				
+				if(msgParts[1].equals("reserve")) {
+					if(reserveBook(bookNum, clientNum)) {
+						returnMsg = ("c" + clientNum + " " + "b" + bookNum).getBytes();
+						continue;
+					}
+					else {
+						returnMsg = ("fail c" + clientNum + " " + "b" + "bookNum").getBytes();
+						continue;
+					}
+				}
+				else if(msgParts[1].equals("return")) {
+					if(returnBook(bookNum, clientNum)) {
+						returnMsg = ("free c" + clientNum + " " + "b" + bookNum).getBytes();
+						continue;
+					}
+					else {
+						returnMsg = ("fail c" + clientNum + " " + "b" + "bookNum").getBytes();
+						continue;
+					}
+				}
+				else {
+					returnMsg = "error".getBytes();
+					continue;
+				}				
+
+			}
+		} catch (SocketException e) {
+			System.err.println(e);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
 		
 	}
+
+	private static synchronized boolean reserveBook (int book, int clientNum) {
+		
+		if(book < 1 || book > bookAvail.length) {
+			return false; //out of range
+		}
+		
+		//-1 because indices 0-9 represent books 1-10, etc.
+		if(bookAvail[book - 1] == 0) {
+			bookAvail[book - 1] = clientNum; //check out book to client
+			return true;
+		}
+		
+		//else book is checked out
+		return false;
+	}
+	
+	private static synchronized boolean returnBook (int book, int clientNum) {
+		
+		if(book < 1 || book > bookAvail.length) {
+			return false; //out of range
+		}
+		
+		if(bookAvail[book - 1] == clientNum) {
+			bookAvail[book - 1] = 0; //return book successfully
+			return true;
+		}
+		
+		//else, something was wrong
+		return false;
+	}
+
 
 }
